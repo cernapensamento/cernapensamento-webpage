8455749f8cb6e048f8ece1395c0c309059eee4a2
Pablo Araujo <pabloarro2005@gmail.com>
2026-07-29 17:55:37 +0200

    migracion

--- src/app/[lang]/error.tsx
+++ src/app/[lang]/error.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect } from 'react';
 import Link from 'next/link';
+import * as Sentry from '@sentry/nextjs';
 
 export default function GlobalError({
   error,
@@ -12,6 +13,7 @@ export default function GlobalError({
 }) {
   useEffect(() => {
     // Aquí podríamos mandar el error a un servicio de telemetría
+    Sentry.captureException(error);
     console.error(error);
   }, [error]);
 
